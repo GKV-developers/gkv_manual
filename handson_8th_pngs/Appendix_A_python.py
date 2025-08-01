a31e6e83ba5f80d5443cfa85e3e0164e4d9871f6
smaeyama <maeyama.shinya@nifs.ac.jp>
2025-08-01 16:21:59 +0900

    Add Appendix A B inpynb

---
+++ handson_8th_pngs/Appendix_A_python.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Pythonの基本の関数と変数、制御文
+# ---
+# 
+# ### (i) 画面への出力
+# print関数を用いる
+# ### (ii) ファイルへの出力
+# テキストファイルを書き込み属性で開き、
+# print関数のオプション変数 file にファイル名を指定する。
+
+# In[ ]:
+
+
+print("Hello world !")
+
+
+# In[ ]:
+
+
+f = open("output.txt",mode="w")
+print("Hello world!", "to the file", file=f)
+f.close()
+
+
+# ### (iii) 関数のヘルプを開く
+
+# In[ ]:
+
+
+help(open)
+
+
+# ### (iv) Pythonにおける変数の取り扱い
+# - 事前の型宣言は必要ない。代入の際に動的に割り付けられる。
+# - あるデータ型を扱っていた変数名を、別のデータ型を上書きすることもできる。
+# - データ型の確認には type 関数を用いる。
+# - Python3では、（整数型）と（整数型）の割り算は自動型変換により実数になる。
+
+# In[ ]:
+
+
+a = 5 + 6
+print(a, type(a))
+a = "word"
+print(a, type(a))
+x = 8 / 5
+print(x, type(x))
+
+
+# ### (v) 制御文
+# - 条件分岐: if-elif-else
+# - ループ: for, while と ループ制御: break, continue
+
+# In[ ]:
+
+
+c = "afternoon"
+if c == "morning":
+    print("朝")
+elif c == "afternoon":
+    print("昼")
+else:
+    print("夜")
+
+
+# In[ ]:
+
+
+a=0
+for i in range(10): # forを利用したループ
+    if i==2:
+        print("# i=2 is skipped")
+        continue  # 処理を中断してループの先頭に戻る
+    a += i
+    print("i=",i,", a=",a)
+    if i>6:
+        print("# When i>6, break the loop")
+        break     # 処理を中断してループから抜け出す
+
+
+# ### (vi) インデント記法
+# Pythonでは、インデント(字下げ)により構文の範囲を決める。{...}やend文で構文の範囲を決め、インデントは自由だったCやFortranとの大きな違い。
+
+# In[ ]:
+
+
+a=0
+for i in range(3):
+    print("i=",i)
+    a += 1  #　ここまでがループ
+print("a=",a)
+
+
+# In[ ]:
+
+
+a=0
+for i in range(3):
+    print("i=",i)
+    a += 1    
+    print("a=",a)#　ここまでがループ 
+
+
+# In[ ]:
+
+
+
+
+
+# # リスト
+# ---
+# 
+# リストは、順序付きのオブジェクトの集まりである。各要素となるオブジェクトは異なる型をとることができる。  
+# len関数・・・リストの長さを表示
+
+# In[ ]:
+
+
+l = [ 1, 2, 3, 8, 1.5, "word", [1,2] ]
+print(l)
+print(type(l))
+print(len(l))
+print(l[6])
+
+
+# ### スライス
+# ・・・リストの一部を取り出す。l[start:stop:step]
+# 
+# - Pythonでは、 l[start:stop] はインデックス start <= i < stop を返す。l[stop] は含まない。
+# - 最初から l[:stop:step] や、最後まで l[start::step] のような省略が可能。
+# - 負のインデックスは末尾から数える。
+
+# In[ ]:
+
+
+print(l[0:3])  # 0 <= i < 3 の要素を取り出す
+
+
+# In[ ]:
+
+
+print(l[0:8:2]) # step=2のインクリメントで、0 <= i < 8までの要素を取り出す。
+
+
+# In[ ]:
+
+
+print(l[:3])    # 最初から i < 3 まで
+print(l[3:])    # 3 <= i から最後まで
+
+
+# In[ ]:
+
+
+#負のインデックスは末尾から数える。
+print(l[-1])
+print(l[-3])
+print(l[-5::3])
+
+
+# In[ ]:
+
+
+#ループ方法1:インデックスを数える
+for i in range(len(l)):
+    print(l[i])
+#ループ方法2:リストの要素に対してもループできる
+for x in l:
+    print(x)
+
---
+++ handson_8th_pngs/Appendix_B_numpy.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 多次元配列の数値計算パッケージ NumPy
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# ## リストのNumPy配列への変換: np.array
+# - np.array 関数により，リストをNumPy 配列に変換できる．この際，リストの各要素には同じ要素数
+# の数値が入っている必要がある．
+# - NumPy 配列の持つndim, shape, dtype などの変数により，配列の次元，形状，数値型(int64: 倍精度
+# 整数, float64: 倍精度実数) が確認できる．
+
+# In[ ]:
+
+
+a = np.array([1,2,3])
+b = np.array([[4,5,6],[7,8,9]])
+c = np.array([a,a,b[0,:],b[1,:]])
+print(type(c))
+print(c)
+print(c.ndim)  # Numpy配列の次元（軸の数）の確認
+print(c.shape) # Numpy配列の形状の確認
+print(c.dtype) # Numpy配列の数値型の確認
+
+
+# In[ ]:
+
+
+# リストの各要素の要素数が異なるなど、多次元配列の形になっていない場合は意図しない動作になる。
+l_mismatch = [[1,2],[3,4],[5,6,7]]
+a_mismatch=np.array(l_mismatch)
+print(type(a_mismatch))
+print(a_mismatch)
+print(a_mismatch.ndim)
+print(a_mismatch.shape)
+print(a_mismatch.dtype)
+
+
+# ## NumPy配列の生成
+# - np.arange([start,] stop[, step]) の関数呼び出しで，start から始まり，step 刻みで増え，stop
+# 未満の等差数列を生成する．step は既定値1，start は既定値0 として省略可能．
+# - np.linspace(start, stop, num) の関数呼び出しで，start から始まり，stop 以下のnum 点の等差
+# 数列が生成される．間隔は(stop-start)/(num-1) となり，終端点stop も含むことに注意．キーワー
+# ド引数endpoint=False を指定すると，間隔(stop-start)/num で，終端点stop を除く等差数列が
+# 生成される．
+
+# In[ ]:
+
+
+i = np.arange(10)                     # 等差整数列の生成
+x = np.arange(0, 2*np.pi, 2*np.pi/10) # 等差実数列の生成1
+xl = np.linspace(0.0, 2*np.pi, 11)    # 等差実数列の生成2
+print(i)
+print(x)
+print(xl)
+
+
+# In[ ]:
+
+
+#関数名の候補を表示する。※*?はjupyter特有の記法
+get_ipython().run_line_magic('psearch', 'np.lin*')
+
+
+# In[ ]:
+
+
+#Numpyのヘルプを表示する。
+help(np.linspace)
+
+
+# ## NumPy配列の演算
+
+# In[ ]:
+
+
+# 配列とスカラーの演算：要素ごとに行われる
+a=np.array([1,2,3,4])
+print(a+1)
+print(2**a)
+# 配列同士の演算：要素ごとに行われる
+b=np.array([2,2,4,4])
+print(a+b)
+print(a*b)
+
+
+# In[1]:
+
+
+#PythonではForループを書かない方が計算が速い。
+from time import time as timer
+a=np.linspace(1,2,100000)
+b=np.linspace(2,4,100000)
+
+t1=timer()
+c1=np.zeros_like(a)
+for i in range(100000):
+    c1[i] = a[i]*b[i]
+t2=timer()
+print("Elapsed time [sec] =",t2-t1)
+
+t1=timer()
+c2=a*b
+t2=timer()
+print("Elapsed time [sec] =",t2-t1)
+
+print(c1==c2)
+
+
+# ## NumPy配列形状の変更
+
+# In[ ]:
+
+
+a=np.arange(24)    # aは 24 個の連続した値をもつ配列
+print(a.shape)
+a=a.reshape(2,12)  # reshapeで指定した配列形状へ変更: 12個の連続した値×2セット
+print(a.shape)
+a=a.reshape(3,2,4) # reshapeで指定した配列形状へ変更: (4個の連続した値×2セット)×3組
+print(a.shape)
+a=a.ravel()        # ravel: 1次元配列へ平坦化
+print(a.shape)
+
+
+# ### 多次元配列のメモリ上の連続性
+# Q. 以下の2次元配列 a を1次元配列へ平坦化(ravel)すると、値の順序はどうなるか？
+
+# In[ ]:
+
+
+a = np.array([[1,3],[2,4]])
+print(a)
+print(a.ravel())
+
+
+# ## NumPy配列の総和・部分和
+# - sum() 関数はNumPy 配列の全要素の和をとる．
+# - 多次元配列の場合，キーワード引数axis を指定することで，指定した軸(左から順に，axis=0,1,2,...)
+# のインデックスについての和をとる．d = a.sum(axis=(1,2)) の例の様に，和をとる軸を複数まとめ
+# て指定することもできる．
+
+# In[ ]:
+
+
+a = np.arange(24)
+a = a.reshape(3,2,4)
+b = a.sum()
+c = a.sum(axis=2)
+d = a.sum(axis=(1,2))
+print(a)
+print(b)
+print(c.shape)
+print(d.shape)
+
+
+# ## Numpy配列を利用した数値積分
+# - np.trapz(y, x[, axis=-1]) 関数は，離散的にサンプリングされた座標を表わす配列x と各座標位置での値y を用いて，定積分
+# $\int^{\max(x)}_{\min(x)} y(x)dx$ を台形公式により数値積分する．
+# - 多次元配列の場合，キーワード引数axis を指定することで，指定した軸について積分する．多変数関
+# 数y(x, t) の積分などで有用．
+
+# In[ ]:
+
+
+x = np.linspace(-5,5,24)
+y = 1/np.sqrt(2*np.pi)*np.exp(-x**2/2)
+area = np.trapz(y,x=x,axis=-1)
+print(area)
+print("Analytic solution: \int_{-∞}^∞ exp(-x^2/2)/√2π = 1")
+
+
+# In[ ]:
+
+
+
+
